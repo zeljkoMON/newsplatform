04b8d1eba48b5d064d6fd3cdcd485ad197147e28
zeljkoMON <zeljkoy@yahoo.com>
2016-04-01 12:27:58 +0200

    testing

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -11,11 +11,8 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use AppBundle\Form\Type\EditUserType;
-use Lcobucci\JWT\Parser;
+
 
 class TestController extends Controller
 {

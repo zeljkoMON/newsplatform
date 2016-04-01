41008ab5226cfd0bb272089110bb28190a7e8b4a
zeljkoMON <zeljkoy@yahoo.com>
2016-04-01 13:34:46 +0200

    testing

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -3,12 +3,6 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Users;
-use AppBundle\Utils\JwtToken;
-use AppBundle\Form\Type\UserType;
-use Lcobucci\JWT\Signer\Hmac\Sha256;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;

2c02adb9d2284d106455d0f0889371dab5c4f9de
zeljkoMON <zeljkoy@yahoo.com>
2016-04-01 13:08:51 +0200

    testing

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -3,19 +3,8 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Users;
-use AppBundle\Utils\JwtToken;
-use AppBundle\Form\Type\UserType;
-use Lcobucci\JWT\Signer\Hmac\Sha256;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use AppBundle\Form\Type\EditUserType;
-use Lcobucci\JWT\Parser;
 
 class TestController extends Controller
 {
